bee08a26671f8b25b76ddc8a0f2fbb01f2fb5e81
Juan Tamayo <juancyepest@gmail.com>
2023-11-30 02:38:06 -0500

    refactor: Update build configuration and ignore files
    Update build configuration to output compiled files to a separate
    'build' directory. Ignore 'build' directory in version control. Modify
    'package.json' to include a 'build' script that removes the existing
    'build' directory and compiles TypeScript files using the 'tsc' command.
    This change improves organization by separating source files from
    compiled files, ensuring cleaner version control and easier deployment.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,12 @@ import express from "express";
 import router from "./controllers/index";
 
 const app = express();
+const port = 3000;
+
 app.disable("x-powered-by");
 
 app.use("/", router);
+
+app.listen(port, () => {
+    console.log(`LISTENING_ON_PORT_${port}`);
+});
