7566d9e4ab07163896c0f8c5f2f8e2c4a489bafa
Juantamayo26 <juancyepest@gmail.com>
2023-12-27 04:16:40 -0500

    finish scheduler

---
+++ reminders-scheduler-rs/README.md
@@ -0,0 +1,12 @@
+## Usage ##
+
+ - 1. `cargo lambda watch --no-reload`
+
+ Open another terminal and run:
+
+ - 2. `cargo lambda invoke reminders-scheduler-rs --data-file exampleLambdaPayload.json`
+
+## Release ##
+
+Run: 
+`cargo lambda deploy --env-file .env`
--- reminders-scheduler-rs/src/main.rs
+++ reminders-scheduler-rs/src/main.rs
@@ -1,9 +1,11 @@
+// TODO: Automate deployments https://www.cargo-lambda.info/guide/automating-deployments.html
 use std::env;
 mod models;
-use std::error::Error;
 
+use aws_lambda_events::event::cloudwatch_events::CloudWatchEvent;
 use dotenv::dotenv;
 use futures::future::join_all;
+use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 use models::reminders::Reminder;
 use sqlx::MySqlPool;
 use whatsapp_cloud_api::{
@@ -11,13 +13,16 @@ use whatsapp_cloud_api::{
     WhatasppClient,
 };
 
-async fn publish_whataspp_reminders(pool: &MySqlPool) -> Result<(), Box<dyn Error>> {
+async fn function_handler(_event: LambdaEvent<CloudWatchEvent>) -> Result<(), Error> {
+    let url = env::var("DATABASE_URL").expect("DATABASE_URL_NOT_FOUND");
+    let pool = MySqlPool::connect(&url).await?;
+
     let token = env::var("WHATSAPP_TOKEN").expect("WHATSAPP_TOKEN_NOT_FOUND");
     let phone_id = env::var("WHATSAPP_PHONE_ID").expect("WHATSAPP_PHONE_ID_NOT_FOUND");
     let whatsapp_client = WhatasppClient::new(token.as_str(), phone_id.as_str());
-    let reminders = Reminder::get_reminders(pool).await?;
+    let reminders = Reminder::get_reminders(&pool).await?;
 
-    println!("{:?}", reminders);
+    println!("REMINDERS: {:?}", reminders);
 
     // TODO: Change this to a template
     let messages: Vec<Message> = reminders
@@ -33,6 +38,7 @@ async fn publish_whataspp_reminders(pool: &MySqlPool) -> Result<(), Box<dyn Erro
         })
         .collect();
 
+    // TODO: TOO MUCH REQUESTS
     let response = join_all(
         messages
             .iter()
@@ -45,13 +51,16 @@ async fn publish_whataspp_reminders(pool: &MySqlPool) -> Result<(), Box<dyn Erro
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<(), Error> {
     dotenv().ok();
 
-    let url = env::var("DATABASE_URL").expect("DATABASE_URL_NOT_FOUND");
-    let conn = MySqlPool::connect(&url).await?;
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        // disable printing the name of the module in every log line.
+        .with_target(false)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        .init();
 
-    publish_whataspp_reminders(&conn).await?;
-
-    Ok(())
+    run(service_fn(function_handler)).await
 }
